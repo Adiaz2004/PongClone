2e8265c3794e3d299af330f2d2050c3c3a56c91a
Adiaz2004 <144147245+Adiaz2004@users.noreply.github.com>
2023-09-12 12:01:10 -0400

    Add files via upload

---
+++ PaddleMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMovement : MonoBehaviour
+{
+    //Variable for movement
+    public float speed = 10f;
+    public float yBorder = 4.5f;
+
+    public GameObject right; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(Input.GetKey(KeyCode.O) && transform.position.y < yBorder) // when O is pressed 
+        {
+        transform.position = new Vector2(transform.position.x, transform.position.y + speed); //move up
+        }
+
+      if (Input.GetKey)
+    }
+}
