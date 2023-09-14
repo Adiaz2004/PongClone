cc691693fd3754790e464a349961bf1bf22513f0
Adiaz2004 <144147245+Adiaz2004@users.noreply.github.com>
2023-09-14 12:01:30 -0400

    Update BallMovement.cs

--- BallMovement.cs
+++ BallMovement.cs
@@ -1,76 +1,104 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BallMovement : MonoBehaviour
 {
-    public float xMoveSpeed = 0; //Variable for x speed
-    public float yMoveSpeed = 0; // cariable for y speed
+    //variable for speed
+    public float xSpeed = 0; //variable for horizontal speed
+    public float ySpeed = 0; //variable for vertical speed
 
-    private float xBorder = 7.5f; // variable for the border of the game
-    private float yBorder = 4.5f; //variable or vertical speed
+    //variable for borders
+    private float xBorder = 7.5f; //variable for horizontal border
+    private float yBorder = 4.5f; //variable for vertical border
 
     //variable for move state
     public bool xMove = true;
     public bool yMove = true;
 
+    //variable for score
+    int playerOneScore;
+    public Text scoreTextp1;
+   
+    int playerTwoScore;
+    public Text scoreTextp2;
 
     // Start is called before the first frame update
     void Start()
     {
-        xMoveSpeed = 0.0125f; // declare value for horizontal speed
-        yMoveSpeed = 0.0125f; // declare value for vertical speed
+        xSpeed = 0.0125f;   //declare value for horizontal speed
+        ySpeed = 0.0125f;   //declare value for vertical speed
     }
-
     // Update is called once per frame
     void Update()
     {
         //horizontal movement
+        if (xMove == true)
+        {
+            transform.position = new Vector2(transform.position.x + xSpeed, transform.position.y); //move right
+        }
+        else
+        {
+            transform.position = new Vector2(transform.position.x - xSpeed, transform.position.y); //move left
+        }
 
-        if(xMove == true)
-        {
-            transform.position = new Vector2(transform.position.x + xMoveSpeed, transform.position.y);// move right
-        }
-        else (yMove == true)
-        {
-            transform.position = new Vector2(transform.position.x - yMoveSpeed, transform.position.y); //move left
-        }
 
         if (transform.position.x >= xBorder)
         {
-            xMove = false; ; //make it go left
+           //Speed = -xSpeed; //make it go left
+            xMove = false; //make it go left
+            playerOneScore += 1;
         }
 
-        if (trasform.position.x <= - xBorder)
+        if (transform.position.x <= -xBorder)
         {
-            xMoveSpeed *= -1;
+           //Speed *= -1;
+            xMove = true;
+            playerTwoScore += 1;
         }
 
-        //Vertical Movement
+        //vertical movement
+        if (yMove == true)
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y + ySpeed); //move up
+        }
 
-        transform.position = new vector2(transform.position.x, transform.position.y + xMoveSpeed);
-        if(transform.position.y >= yBorder)
+        else
         {
-            yMoveSpeed *= -1;
+            transform.position = new Vector2(transform.position.x, transform.position.y - ySpeed);   //move down
         }
-        if(transform.positiony <= -yBorder)
+        if (transform.position.y >= yBorder)
         {
-            yMoveSpeed *= -1;
+         // ySpeed *= -1;
+            yMove = false;
+           
         }
+
+        if (transform.position.y <= -yBorder)
+        {
+          //ySpeed *= -1;
+            yMove = true;
+        }
+
+        scoreTextp1.text = playerOneScore.ToString();
+
+        scoreTextp2.text = playerTwoScore.ToString();
+
     }
 
-    void OnCollisionEnter2D(Collision2D collision) //when a collision happen
+    void OnCollisionEnter2D(Collision2D collision) //when a collision happens
     {
-        debug.log("hit");
+        Debug.Log("hit");
         if (collision.collider.CompareTag("Player"))
         {
             if (xMove == true)
-            { 
-                xMove = false
+            {
+                xMove = false;
             }
             else if (xMove == false)
             {
-                xMove = true; 
+                xMove = true;
             }
         }
     }
